0dd671dab07ce43df5697e1fcad7b8d91e1bd057
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-01-19 18:44:47 -0500

    Creando modelos de base de datos con Sequelize - Postgres (Pokemon y Tipos)

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,60 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define("pokemon", {
+    id: {
+      type: DataTypes.UUID,
+      valor: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    life: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    force: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    height: {
+      //altura
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      //peso
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    img: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    idapi: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    addtobd: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
     },
   });
 };
---
+++ api/src/models/Tipos.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("tipos", {
+    id: {
+      type: DataTypes.UUID,
+      valor: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
