39b70fddf6d5fd9ff2c68aa81b0820860375019a
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-02-01 19:12:29 -0500

    Update...

--- client/src/Componentes/Footer.jsx
+++ client/src/Componentes/Footer.jsx
@@ -6,33 +6,39 @@ import Li from "../../src/Icos/linked.png";
 import Insta from "../../src/Icos/insta.png";
 import Web from "../../src/Icos/web.png";
 export default function Footer() {
-  return (
-    <div className="footer">
-      <h2>Proyecto Pokex</h2>
-      <p>
-        Realizado por{" "}
-        <a href="https://www.jeanoviedo.com" target="_blank">
-          Jean Oviedo
+    return (<div className="footer">
+        <h2>Proyecto Pokex</h2>
+        <p>
+            Realizado por{" "}
+            <a href="https://www.jeanoviedo.com" target="_blank">
+                Jean Oviedo
+            </a>
+            , es una app que muestra información de pokemones esta  fue creada con
+                    JavaScript, CSS, React, Redux con la API de POKEAPI complementando con una base de datos utilizando Express,
+             Sequelize y Postgres.
+            <br></br>
+        </p>
+        <br/>
+
+        <a href="https://github.com/JeanOviedo" target="_blank">
+            <img src={Git}
+                className="ico"
+                alt="..."/>
         </a>
-        , es una app que muestra información de pokemones, entrenadores y cuenta
-        con una tienda de articulos referente a pokemon fue creada con
-        JavaScript, CSS, React, Redux con la API de POKEAPI.
-        <br></br>
-      </p>
-      <br />
-     
-      <a href="https://github.com/JeanOviedo" target="_blank">
-        <img src={Git} className="ico" alt="..." />
-      </a>
-      <a href="https://www.linkedin.com/in/jean-oviedo/" target="_blank">
-        <img src={Li} className="ico" alt="..." />
-      </a>
-      <a href="https://www.instagram.com/jeanoviedolopez/" target="_blank">
-        <img src={Insta} className="ico" alt="..." />
-      </a>
-      <a href="https://www.jeanoviedo.com" target="_blank">
-        <img src={Web} className="ico" alt="..." />
-      </a>
-    </div>
-  );
+        <a href="https://www.linkedin.com/in/jean-oviedo/" target="_blank">
+            <img src={Li}
+                className="ico"
+                alt="..."/>
+        </a>
+        <a href="https://www.instagram.com/jeanoviedolopez/" target="_blank">
+            <img src={Insta}
+                className="ico"
+                alt="..."/>
+        </a>
+        <a href="https://www.jeanoviedo.com" target="_blank">
+            <img src={Web}
+                className="ico"
+                alt="..."/>
+        </a>
+    </div>);
 }
